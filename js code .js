f729dce165a1ce9e28c5435e044b689a05515210
sakshamagrawall <sakshamagrawal18@gmail.com>
2022-11-02 23:50:34 +0530

    adding js file

---
+++ js code .js
@@ -0,0 +1,11 @@
+//1.Accessing a veriable
+const num = 5;
+console.log(num);
+
+//2.Declare a varable and assign a number
+console.log('The value of num is: ' + num);
+
+//3.Reassign a value to a variable
+let text = 'hello';
+text = 'bye';
+console.log(text);
