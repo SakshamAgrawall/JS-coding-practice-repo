3e8d5b06f8cc9fcf9e922a9ed20bc7b3c1b26464
sakshamagrawall <sakshamagrawal18@gmail.com>
2022-11-03 20:02:39 +0530

    adding operators file

---
+++ operators/operators.js
@@ -0,0 +1,89 @@
+//1.Comparison operatos - Equal
+const num1 = 5;
+const num2 = 5;
+console.log(num1 == num2);
+
+//2.Comparison operatos - Not equal
+const a = 5;
+const b = 6;
+console.log(a != b);
+
+//3.Comparison operators - Greater than
+const A = 6;
+const B = 5;
+console.log(A > B);
+
+//4.Comparison operators - less than
+const p = 1;
+const q = 2;
+console.log(p < q);
+
+//5.Comparsion operators - Greater than or equal
+const r = 3;
+const m = 2;
+const n = 2;
+console.log(r >= m, m >= n);
+
+//6.Comparsion operators - less than or equal
+const  w= 1;
+const x = 1;
+const y = 2;
+console.log(w <= x, x <= y);
+
+//7.Comparison operators - Strict equality
+const num3='5';
+const num4='5';
+console.log(num3===num4);
+
+//8.Comparison operators - Strict inequality
+const num6="5";
+const num7=5;
+console.log(num6 !== num7);
+
+//9.Logical operators - Logical And
+const num8 = 13;
+const num9 = 13;
+result = num8 > 12 && num9 > 12;
+console.log(result);
+
+//10.Logical operatos- logical OR
+const num10 = 13;
+const num11 = 13;
+result1 = num10 > 12 || num11 > 12;
+console.log(result1);
+
+//11.Arithmetic operators - Addition
+const num14 = 4;
+const num15 = 6;
+const sum = num14 + num15;
+console.log(sum == 10);
+
+//12.Arithmetic operators - Subtraction
+const num16 = 9;
+const num17 = 4;
+const dif = num16 - num17;
+console.log(dif == 5);
+
+//13.Arithmetic operators - Multiplication
+const num18 = 9;
+const num19 = 3;
+const product = num18 * num19;
+console.log(product == 81);
+
+//14.Arithmetic operators - Division
+const num20 = 81;
+const num21 = 9;
+const quotient = num20 / num21;
+console.log(quotient == 9);
+
+//15.Arithmetic operators - Exponential
+const numOne = 2;
+const numTwo = 3;
+const res = numOne**numTwo;
+console.log(res == 8);
+
+//16.Arithmetic operators - Remainder
+const num24 = 13;
+const num25 = 2;
+resultt = num24%num25;
+console.log(resultt);
