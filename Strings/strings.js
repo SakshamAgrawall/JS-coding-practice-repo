8a9c6dd5ae777766c24c0107166c2e45c026c028
sakshamagrawall <sakshamagrawal18@gmail.com>
2022-11-03 20:21:42 +0530

    adding strings file

---
+++ Strings/strings.js
@@ -0,0 +1,15 @@
+//connect javascript string (1)
+const text1 = 'Hello';
+const text2 = 'World';
+const combined = text1 + ' ' + text2;
+console.log(combined);
+
+//Connect Javascript strings (2)
+const name = `Peter`;
+const name1 = `Jack`;
+const result = `The names are ${name} and ${name1}`;
+console.log(result);
+
+//treating string as array
+const a = 'hello';
+console.log(a[4]);
